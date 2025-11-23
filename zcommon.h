1f0d84c7d5b85ad9d23184565d68895c705c2d32
Zuhaitz <zuhaitz.zechhub@gmail.com>
2025-11-23 22:09:39 +0000

    Add zcommon.h

---
+++ zcommon.h
@@ -0,0 +1,21 @@
+
+#ifndef ZCOMMON_H
+#define ZCOMMON_H
+
+#include <stddef.h>
+
+// Return Codes.
+#define Z_OK     0
+#define Z_ERR   -1
+#define Z_FOUND  1
+
+// Memory Macros.
+// If the user hasn't defined their own allocator, use the standard one.
+#ifndef Z_MALLOC
+    #include <stdlib.h>
+    #define Z_MALLOC(sz)       malloc(sz)
+    #define Z_REALLOC(p, sz)   realloc(p, sz)
+    #define Z_FREE(p)          free(p)
+#endif
+
+#endif
